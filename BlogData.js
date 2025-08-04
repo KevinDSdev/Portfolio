5e987d738c7077ea663691b044af1e268c9af34d
Kevin <kevindesilva34@gmail.com>
2025-08-04 21:04:33 +0530

    initial

---
+++ BlogData.js
@@ -0,0 +1,15 @@
+export const BlogDatas = [
+{
+    id:0,
+    Name:'Blog test1',
+    ImageName:'BLogTest'
+
+},
+{
+    id:1,
+    Name:'Blog test2',
+    ImageName:'BLogTest'
+
+},
+
+]
--- scripts.js
+++ scripts.js
@@ -1,45 +1,23 @@
-document.getElementById('LearnMoreButtonID').addEventListener('click', function (e) {
-  e.preventDefault();
+
+
+function GoToAbout(e){
+    e.preventDefault();
   const target = document.querySelector('#About');
   if (target) {
     target.scrollIntoView({ behavior: 'smooth' });
   }
-});
-document.getElementById('AboutScrollBTN').addEventListener('click', function (e) {
-  e.preventDefault();
-  const target = document.querySelector('#About');
-  if (target) {
-    target.scrollIntoView({ behavior: 'smooth' });
-  }
-});
-document.getElementById('AboutScrollBTN2').addEventListener('click', function (e) {
-  e.preventDefault();
-  const target = document.querySelector('#About');
-  if (target) {
-    target.scrollIntoView({ behavior: 'smooth' });
-  }
-});
-document.getElementById('SkillsButtonID').addEventListener('click', function (e) {
-  e.preventDefault();
+}
+
+
+function GoToSKills(e){
+   e.preventDefault();
   const target = document.querySelector('#Skills');
   if (target) {
     target.scrollIntoView({ behavior: 'smooth' });
   }
-});
-document.getElementById('SkillsButtonID2').addEventListener('click', function (e) {
-  e.preventDefault();
-  const target = document.querySelector('#Skills');
-  if (target) {
-    target.scrollIntoView({ behavior: 'smooth' });
-  }
-});
-document.getElementById('SkillsButtonID3').addEventListener('click', function (e) {
-  e.preventDefault();
-  const target = document.querySelector('#Skills');
-  if (target) {
-    target.scrollIntoView({ behavior: 'smooth' });
-  }
-});
+}
+
+
 
 function toggleSidebar() {
   const sidebar = document.getElementById("SlideinMenuID");
@@ -49,10 +27,11 @@ function toggleSidebar() {
     sidebar.style.left = "0%";
   }
 }
-document.getElementById('Home').addEventListener('click', function (e) {
-  e.preventDefault();
+
+function GoToHome(e){
+ e.preventDefault();
   const target = document.querySelector('#HomeID');
   if (target) {
     target.scrollIntoView({ behavior: 'smooth' });
   }
-});
+}
