8b1add843354deed05b160081cf7bd5b69949769
Kevin <kevindesilva34@gmail.com>
2025-08-04 20:01:05 +0530

    initial

--- scripts.js
+++ scripts.js
@@ -12,6 +12,13 @@ document.getElementById('AboutScrollBTN').addEventListener('click', function (e)
     target.scrollIntoView({ behavior: 'smooth' });
   }
 });
+document.getElementById('AboutScrollBTN2').addEventListener('click', function (e) {
+  e.preventDefault();
+  const target = document.querySelector('#About');
+  if (target) {
+    target.scrollIntoView({ behavior: 'smooth' });
+  }
+});
 document.getElementById('SkillsButtonID').addEventListener('click', function (e) {
   e.preventDefault();
   const target = document.querySelector('#Skills');
@@ -19,5 +26,26 @@ document.getElementById('SkillsButtonID').addEventListener('click', function (e)
     target.scrollIntoView({ behavior: 'smooth' });
   }
 });
+document.getElementById('SkillsButtonID2').addEventListener('click', function (e) {
+  e.preventDefault();
+  const target = document.querySelector('#Skills');
+  if (target) {
+    target.scrollIntoView({ behavior: 'smooth' });
+  }
+});
+document.getElementById('SkillsButtonID3').addEventListener('click', function (e) {
+  e.preventDefault();
+  const target = document.querySelector('#Skills');
+  if (target) {
+    target.scrollIntoView({ behavior: 'smooth' });
+  }
+});
 
-
+function toggleSidebar() {
+  const sidebar = document.getElementById("SlideinMenuID");
+  if (sidebar.style.left === "0%") {
+    sidebar.style.left = "-100%";
+  } else {
+    sidebar.style.left = "0%";
+  }
+}
